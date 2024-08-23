3e05e25a71b7aa82bc75ebb982f42039dd2aecc3
Thaylla <167567044+TataFaria@users.noreply.github.com>
2024-08-23 14:51:57 -0300

    Adicionando a logo e centralizando os icones do Navbar

--- front-end/src/Components/Sections/NavBar.jsx
+++ front-end/src/Components/Sections/NavBar.jsx
@@ -1,14 +1,17 @@
 import Nav from 'react-bootstrap/Nav';
 import styles from './NavBar.module.css';
 
+
 function Navbar() {
+    
     return(
         <div className={styles.navbar}>
             <ul>
-                <li><Nav.Link href='#Adote'>Adote</Nav.Link></li>
+                <li><Nav.Link href='#Adote' className={styles.adoteLink}>Adote</Nav.Link></li>
                 <li><Nav.Link href='#Apadrinhe'>Apadrinhe</Nav.Link></li>
                 <li><Nav.Link href='#SejaVoluntario'>Seja Voluntário</Nav.Link></li>
            </ul>
+           
         </div>
     )
 }
