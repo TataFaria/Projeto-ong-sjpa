778f460e89a59c68f0cd3f0d5838b28a59f23084
Thaylla <167567044+TataFaria@users.noreply.github.com>
2024-08-26 17:54:54 -0300

    Criando a Section NossosPets

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Sections/NavBar'
 import Adote from './Components/Sections/Adote'
 import Apadrinhe from './Components/Sections/Apadrinhe'
 import SejaVoluntario from './Components/Sections/SejaVoluntario'
+import NossosPets from './Components/Sections/NossosPets'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Adote/>
       <Apadrinhe/>
       <SejaVoluntario/>
+      <NossosPets/>
    </div>
       
   )
---
+++ front-end/src/Components/Sections/NossosPets.jsx
@@ -0,0 +1,11 @@
+import styles from './NossosPets.module.css';
+
+function NossosPets() {
+    return (
+        <div className={styles.NossosPets}>
+            <h1> Nossos Pets </h1>
+        </div>
+    );
+}
+
+export default NossosPets;
