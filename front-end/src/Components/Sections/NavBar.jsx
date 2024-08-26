db360a0a9666b760739a01f352bd34440e078ed9
Maria Fernanda <mfmelo.de@gmail.com>
2024-08-26 09:41:43 -0300

    Adicionei mais um link no Navbar SobreNos

--- front-end/src/Components/Sections/NavBar.jsx
+++ front-end/src/Components/Sections/NavBar.jsx
@@ -1,19 +1,29 @@
-import Nav from 'react-bootstrap/Nav';
-import styles from './NavBar.module.css';
-
+import Nav from "react-bootstrap/Nav";
+import styles from "./NavBar.module.css";
 
 function Navbar() {
-    
-    return(
-        <div className={styles.navbar}>
-            <ul>
-                <li><Nav.Link href='#Adote' className={styles.adoteLink}>Adote</Nav.Link></li>
-                <li><Nav.Link href='#Apadrinhe'>Apadrinhe</Nav.Link></li>
-                <li><Nav.Link href='#SejaVoluntario'>Seja Voluntário</Nav.Link></li>
-           </ul>
-           
-        </div>
-    )
+  return (
+    <div className={styles.navbar}>
+      <ul>
+        <li>
+          <Nav.Link href="#SobreNós" className={styles.adoteLink}>
+            Sobre Nós
+          </Nav.Link>
+        </li>
+        <li>
+          <Nav.Link href="#Adote" className={styles.adoteLink}>
+            Adote
+          </Nav.Link>
+        </li>
+        <li>
+          <Nav.Link href="#Apadrinhe">Apadrinhe</Nav.Link>
+        </li>
+        <li>
+          <Nav.Link href="#SejaVoluntario">Seja Voluntário</Nav.Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
