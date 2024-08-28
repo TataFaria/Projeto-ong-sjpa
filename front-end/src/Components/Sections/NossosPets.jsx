2a1655265de07252fc1b034142dd568851d0d789
Thaylla <167567044+TataFaria@users.noreply.github.com>
2024-08-28 08:49:12 -0300

    Resolvendo conflito

--- front-end/src/Components/Sections/NossosPets.jsx
+++ front-end/src/Components/Sections/NossosPets.jsx
@@ -1,56 +1,55 @@
 import styles from './NossosPets.module.css';
-import dogBela from '../Imagens/dogBela.svg';
 
 function NossosPets() {
     return (
         <div className={styles.NossosPets}>
-            <h1> Nossos Pets </h1>
-            <div className={styles.TiposPets}>
-                <ul>
-                    <li>Gatos</li>
-                    <li>Cachorros</li>
-                    <li>Todas as espécies</li>
-                </ul>
+        <h1> Nossos Pets </h1>
+        <div className={styles.TiposPets}>
+            <ul>
+                <li>Gatos</li>
+                <li>Cachorros</li>
+                <li>Todas as espécies</li>
+            </ul>
+        </div>
+        <div className={styles.SectionCards}>
+            <div className={styles.CardsPet}>
+               
+                <h2>Bela</h2>
+                <p>Adulto / Pequeno</p>
+                <button>Saiba Mais</button>
             </div>
-            <div className={styles.SectionCards}>
-                <div className={styles.CardsPet}>
-                    <img src={dogBela} alt="" />
-                    <h2>Bela</h2>
-                    <p>Adulto / Pequeno</p>
-                    <button>Saiba Mais</button>
-                </div>
-                <div className={styles.CardsPet}>
-                    <img src={dogBela} alt="" />
-                    <h2>Bela</h2>
-                    <p>Adulto / Pequeno</p>
-                    <button>Saiba Mais</button>
-                </div>
-                <div className={styles.CardsPet}>
-                    <img src={dogBela} alt="" />
-                    <h2>Bela</h2>
-                    <p>Adulto / Pequeno</p>
-                    <button>Saiba Mais</button>
-                </div>
-                <div className={styles.CardsPet}>
-                    <img src={dogBela} alt="" />
-                    <h2>Bela</h2>
-                    <p>Adulto / Pequeno</p>
-                    <button>Saiba Mais</button>
-                </div>
-                <div className={styles.CardsPet}>
-                    <img src={dogBela} alt="" />
-                    <h2>Bela</h2>
-                    <p>Adulto / Pequeno</p>
-                    <button>Saiba Mais</button>
-                </div>
-                <div className={styles.CardsPet}>
-                    <img src={dogBela} alt="" />
-                    <h2>Bela</h2>
-                    <p>Adulto / Pequeno</p>
-                    <button>Saiba Mais</button>
-                </div>
+            <div className={styles.CardsPet}>
+             
+                <h2>Bela</h2>
+                <p>Adulto / Pequeno</p>
+                <button>Saiba Mais</button>
+            </div>
+            <div className={styles.CardsPet}>
+               
+                <h2>Bela</h2>
+                <p>Adulto / Pequeno</p>
+                <button>Saiba Mais</button>
+            </div>
+            <div className={styles.CardsPet}>
+                
+                <h2>Bela</h2>
+                <p>Adulto / Pequeno</p>
+                <button>Saiba Mais</button>
+            </div>
+            <div className={styles.CardsPet}>
+              
+                <h2>Bela</h2>
+                <p>Adulto / Pequeno</p>
+                <button>Saiba Mais</button>
+            </div>
+            <div className={styles.CardsPet}>
+            
+                <h2>Bela</h2>
+                <p>Adulto / Pequeno</p>
+                <button>Saiba Mais</button>
             </div>
         </div>
+    </div>
     );
 }
 
--- front-end/src/Components/Sections/SobreNos.jsx
+++ front-end/src/Components/Sections/SobreNos.jsx
@@ -12,7 +12,7 @@ function SobreNos() {
                 carinho.</p>
             </div>
         </div>
-    );
+    )
 }
 
 export default SobreNos;
